acf26edd4ea68bd41abcaefa1bcee11ecb460301
Enndfp <3193947435@qq.com>
2023-08-08 20:38:54 +0800

    编译期优化

---
+++ demo/src/main/java/com/enndfp/class_loader_test/Demo6.java
@@ -0,0 +1,19 @@
+package com.enndfp.class_loader_test;
+
+/**
+ * 编译期优化
+ *
+ * @author Enndfp
+ */
+public class Demo6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 200; i++) {
+            long start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+                new Object();
+            }
+            long end = System.nanoTime();
+            System.out.printf("%d\t%d\n", i, (end - start));
+        }
+    }
+}
